bcba2573c058de984c9bc21385037f3e3c967285
Cyrus Khalatbari <cyruskhalatbari@Cyruss-MBP.lan.hesge.ch>
2023-10-24 17:42:11 +0200

    Feat: updating the index.js code

--- index.js
+++ index.js
@@ -3,25 +3,30 @@ var axios = require('axios');
 const app = express()
 const port = process.env.PORT || 3000;
 
-let config = {};
-
+// Here, we create a variable for the data we want to send over the API; 
 let issPositionData = null;
 
-axios.get('http://api.open-notify.org/iss-now.json', config)
+// Here, we call the thirdparty API (e.g: the ISS position) and assign its result to our variable
+axios.get('http://api.open-notify.org/iss-now.json')
 .then((response) => {
     issPositionData = response.data.iss_position;
 });
 
+// Here, we create our first route (called when we reach the /) 
 app.get('/', (req, res) => {
     let stringifiedIssPositionData = JSON.stringify(issPositionData);
+    // res.send() is what is used to send the response through the API
+    // The ${} is a template string, for more: see on the MDN web docs. 
     res.send(`The full position of the ISS is ${stringifiedIssPositionData}`)
 })
 
+// Here, we create our second route (called when we reach the /latitude) 
 app.get('/latitude', (req, res) => {
     let issPositionLatitude = issPositionData.latitude;
     res.send(`The latitude position of the ISS is ${issPositionLatitude}`)
 })
 
+// Here, we create our third route (called when we reach the /latitude) 
 app.get('/longitude', (req, res) => {
     let issPositionLongitude = issPositionData.longitude;
     res.send(`The longitude position of the ISS is ${issPositionLongitude}`)
